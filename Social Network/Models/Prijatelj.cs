deb74dcd8e56af9b79f43af26e3618c4e6ca8589
Anesa Drakovac <adrakovac1@etf.unsa.ba>
2016-03-22 23:40:40 +0100

    Model - Prijatelj property

--- Social Network/Models/Prijatelj.cs
+++ Social Network/Models/Prijatelj.cs
@@ -8,7 +8,7 @@ namespace Social_Network.Models
     public class Prijatelj
     {
         public int Id { get; set; }
-        int ProfilId;
-        DateTime prijateljiOd;
+        public int ProfilId { get; set; }
+        public DateTime prijateljiOd { get; set; }
     }
 }
