f405aa352a6c86d2b5aa9339f2cd9936f6a0984c
Anesa Drakovac <adrakovac1@etf.unsa.ba>
2016-03-22 23:33:47 +0100

    Model - Notifikacija property

--- Social Network/Models/Notifikacija.cs
+++ Social Network/Models/Notifikacija.cs
@@ -8,9 +8,9 @@ namespace Social_Network.Models
     public class Notifikacija
     {
         public int Id { get; set; }
-        int ProfilId;
-        string poruka;
-        DateTime vrijeme;
-        string urlObjave;
+        public int ProfilId { get; set; }
+        public string poruka { get; set; }
+        public DateTime vrijeme { get; set; }
+        public string urlObjave { get; set; }
     }
 }
