b3512dbfda55dfe12e9c51a8315705056168b8f3
Anesa Drakovac <adrakovac1@etf.unsa.ba>
2016-03-22 23:43:23 +0100

    Model - Profil property

--- Social Network/Models/Profil.cs
+++ Social Network/Models/Profil.cs
@@ -8,13 +8,13 @@ namespace Social_Network.Models
     public class Profil
     {
         public int Id { get; set; }
-        int OsobaId;
-        string username;
-        string password;
-        string email;
-        string slika;
-        bool aktivan;
-        DateTime registrovan;
-        bool administrator;
+        public int OsobaId { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+        public string slika { get; set; }
+        public bool aktivan { get; set; }
+        public DateTime registrovan { get; set; }
+        public bool administrator { get; set; }
     }
 }
