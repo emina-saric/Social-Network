c41f8df19bf17b604458758672e4255bafb4ea1e
Anesa Drakovac <adrakovac1@etf.unsa.ba>
2016-03-22 23:38:05 +0100

    Model - Osoba property

--- Social Network/Models/Osoba.cs
+++ Social Network/Models/Osoba.cs
@@ -8,12 +8,12 @@ namespace Social_Network.Models
     public class Osoba
     {
         public int Id { get; set; }
-        string ime;
-        string prezime;
-        DateTime datumRodjenja;
-        string drzava;
-        string grad;
-        string spol;
-        string telefon;
+        public string ime { get; set; }
+        public string prezime { get; set; }
+        public DateTime datumRodjenja { get; set; }
+        public string drzava { get; set; }
+        public string grad { get; set; }
+        public string spol { get; set; }
+        public string telefon { get; set; }
     }
 }
