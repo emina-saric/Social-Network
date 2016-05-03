eb9cfd840de357ec56ef0b4bfbef700a9e09da57
Emina Šarić <esaric1@etf.unsa.ba>
2016-05-03 13:56:09 +0200

    Dadano polje za mail

--- Social Network/Models/UserModel.cs
+++ Social Network/Models/UserModel.cs
@@ -29,8 +29,9 @@ namespace Social_Network.Models
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
         public string LastName { get; set; }
-
-
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
+        public string Email { get; set; }
     }
 
    
