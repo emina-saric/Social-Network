8fe7ec2ec5f0c4c28bc1222bd61c96db8c7a894f
Armin Klačar <aklacar1@etf.unsa.ba>
2016-05-10 17:24:49 +0200

    SearchController fixed

--- SocialNetwork.Web/app/controllers/SearchController.js
+++ SocialNetwork.Web/app/controllers/SearchController.js
@@ -1,10 +1,10 @@
 ﻿'use strict';
 app.controller('searchController', ['$scope', '$location', '$timeout', 'authService','$routeParams','searchService', function ($scope, $location, $timeout, authService,$routeParams,searchService) {
 
-                    
-
-           
-/*$scope.people = [
+    $scope.people = new Array();
+    var profili = new Array();
+       /*
+$scope.people = [
     { FirstName: 'John ', LastName: 'Doe', picture: 'http://www.saintsfc.co.uk/images/common/bg_player_profile_default_big.png' },
     { FirstName: 'Axel ', LastName: 'Zarate', picture: 'https://avatars0.githubusercontent.com/u/4431445?s=60' },
     { FirstName: 'Walter White', LastName: 'White', picture: 'http://upstreamideas.org/wp-content/uploads/2013/10/ww.jpg' },
@@ -13,12 +13,24 @@ app.controller('searchController', ['$scope', '$location', '$timeout', 'authServ
 { FirstName: 'Axel Zarate', LastName: 'Zarate', picture: 'https://avatars0.githubusercontent.com/u/4431445?s=60' },
 { FirstName: 'John Doe', LastName: 'Doe', picture: 'http://www.saintsfc.co.uk/images/common/bg_player_profile_default_big.png' }
 ];*/
-var profili = searchService.getAllUsers();
-for (var i = 0; i < profili.length; i++) {
-    var profil = [{ FirstName: profili[i].FirstName, LastName: profili[i].LastName, picture: 'http://upstreamideas.org/wp-content/uploads/2013/10/ww.jpg' }];
-    $scope.people.concat(profil);
-    console.log($scope.people[i].FirstName);
-}
-                    
-                   
+
+
+    $scope.getAllUsers = function () {
+        searchService.getAllUsers().then(function (response) {
+            profili = response.data;
+            for (var i = 0; i < profili.length; i++) {
+
+                var profil = {
+                    FirstName: "",
+                    LastName: "",
+                    picture: ""
+                };
+                profil.FirstName = profili[i].firstName;
+                profil.LastName = profili[i].lastName;
+                profil.picture = "http://upstreamideas.org/wp-content/uploads/2013/10/ww.jpg";
+                $scope.people.push(profil);
+            }
+        });
+    };
+    $scope.getAllUsers();             
 }]);
